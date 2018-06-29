98205bcb6ecbc464664f1d8af18bcf5ea1f9effd
Ashish <ashiz2013@gmail.com>
2018-06-30 00:09:04 +0545

    [SETUP] Template code base for django with layout

--- krishisathi/krishiapp/views.py
+++ krishisathi/krishiapp/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def about(request):
+    return render(request, 'about.html')
--- krishisathi/krishisathi/urls.py
+++ krishisathi/krishisathi/urls.py
@@ -20,5 +20,8 @@ from krishiapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('about', views.about, name='about'),
 ]
