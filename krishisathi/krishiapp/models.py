24364c1dceea7eea930e3920b71436e2481311d1
Ashish <ashiz2013@gmail.com>
2018-06-30 02:07:01 +0545

    [CREATE] UserProfile model along with its migration

--- krishisathi/krishiapp/models.py
+++ krishisathi/krishiapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -6,4 +7,6 @@ from django.db import models
 class UserProfile(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    contact = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
