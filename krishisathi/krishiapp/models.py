05fb2a3bc802080b4f73ca6c221033ab2c48cd0e
Ashish <ashiz2013@gmail.com>
2018-06-30 02:02:18 +0545

    [ADD] Form fields for login and sign up form

--- krishisathi/krishiapp/models.py
+++ krishisathi/krishiapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
