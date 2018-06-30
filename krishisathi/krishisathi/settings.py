0cca3d9c0b5a86687938cabe9b5999287c18931b
Ashish <ashiz2013@gmail.com>
2018-06-30 12:11:41 +0545

    [ADD] Python decouple and separate .env example file

--- krishisathi/krishisathi/settings.py
+++ krishisathi/krishisathi/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'krishiapp/templates')
@@ -21,10 +23,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'krishiapp/templates')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a!yrg%pmkg(ndw2pig%j%fvyn#66(r@=fpai!$q@q&ty47c*v#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -79,8 +81,12 @@ WSGI_APPLICATION = 'krishisathi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
