bdb82cb79d363dd4188941e2e8a6449a5c8a5e3b
Ashish <ashiz2013@gmail.com>
2018-06-30 11:42:35 +0545

    [ADD] Userreadings model and its migrations

--- krishisathi/krishiapp/models.py
+++ krishisathi/krishiapp/models.py
@@ -10,3 +10,12 @@ class UserProfile(models.Model):
     contact = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+class UserReadings(models.Model):
+    ph = models.FloatField()
+    temp = models.FloatField()
+    humidity = models.FloatField()
+    moisture = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
