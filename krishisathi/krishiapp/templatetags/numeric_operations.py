ab6403ae49078db3d0255b05d185eb4394129b32
Ashish <ashiz2013@gmail.com>
2018-07-16 16:00:06 +0545

    [FIX] Divide by zero errors in templatetag

--- krishisathi/krishiapp/templatetags/numeric_operations.py
+++ krishisathi/krishiapp/templatetags/numeric_operations.py
@@ -5,14 +5,14 @@ register = template.Library()
 
 @register.filter(name='find_mean')
 def find_mean(value):
-    return round(sum(value) / float(len(value)), 2)
+    return round(sum(value) / float(len(value)), 2) if value else 0.0
 
 
 @register.filter(name='find_max')
 def find_max(value):
-    return round(max(value), 2)
+    return round(max(value), 2) if value else 0.0
 
 
 @register.filter(name='find_min')
 def find_min(value):
-    return round(min(value), 2)
+    return round(min(value), 2)  if value else 0.0
