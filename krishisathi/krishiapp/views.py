c4fcf3cc51964fb73767733c2470080a8d153847
Ashish <ashiz2013@gmail.com>
2018-07-23 20:38:48 +0545

    [UPDATE] Limit the number of classifiers to three

--- krishisathi/krishiapp/views.py
+++ krishisathi/krishiapp/views.py
@@ -89,7 +89,7 @@ def profile(request):
         pass
     classifiers = list(counter.keys())
     if len(classifiers) > 3:
-        classifiers = classifiers[0:2]
+        classifiers = classifiers[0:3]
     context = {
         'ph_readings': ph_readings,
         'temp_readings': temp_readings,
